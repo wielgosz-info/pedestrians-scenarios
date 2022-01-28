32618d53769a6c45d16a884d1c26c34f4bfc802d
Maciej Wielgosz <wielgosz@agh.edu.pl>
2022-01-28 15:39:55 +0100

    Add scenario_runner submodule

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,23 @@ FROM wielgoszinfo/pedestrians-common:${PLATFORM}-latest AS base
 
 ENV PACKAGE=pedestrians-scenarios
 
+# Install direct dependencies and scenario_runner ones
+RUN /venv/bin/python -m pip install --no-cache-dir \
+    carla==0.9.13 \
+    ephem==4.1.3 \
+    matplotlib==3.5.1 \
+    networkx==2.2 \
+    numpy==1.22.1 \
+    opencv-python-headless==4.2.0.32 \
+    Pillow==9.0.0 \
+    psutil==5.9.0 \
+    py-trees==0.8.3 \
+    Shapely==1.7.1 \
+    simple-watchdog-timer==0.1.1 \
+    six==1.16.0 \
+    tabulate==0.8.9 \
+    xmlschema==1.0.18
+
 
 # Copy client files so that we can do editable pip install
 COPY --chown=${USERNAME}:${USERNAME} . /app
--- src/pedestrians_scenarios/skeleton.py
+++ src/pedestrians_scenarios/skeleton.py
@@ -33,29 +33,6 @@ __license__ = "MIT"
 _logger = logging.getLogger(__name__)
 
 
-# ---- Python API ----
-# The functions defined in this section can be imported by users in their
-# Python scripts/interactive interpreter, e.g. via
-# `from pedestrians_scenarios.skeleton import fib`,
-# when using this Python module as a library.
-
-
-def fib(n):
-    """Fibonacci example function
-
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for _i in range(n - 1):
-        a, b = b, a + b
-    return a
-
-
 # ---- CLI ----
 # The functions defined in this section are wrappers around the main Python
 # API allowing them to be called directly from the terminal as a CLI
@@ -111,20 +88,8 @@ def setup_logging(loglevel):
 
 
 def main(args):
-    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
-
-    Instead of returning the value from :func:`fib`, it prints the result to the
-    ``stdout`` in a nicely formatted message.
-
-    Args:
-      args (List[str]): command line parameters as list of strings
-          (for example  ``["--verbose", "42"]``).
-    """
     args = parse_args(args)
     setup_logging(args.loglevel)
-    _logger.debug("Starting crazy calculations...")
-    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
-    _logger.info("Script ends here")
 
 
 def run():
